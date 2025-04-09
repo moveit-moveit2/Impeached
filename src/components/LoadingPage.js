91de3feb50e17ee74dd00f59aed0fff4ed0eab09
moveit-moveit2 <lilycocochoi@gmail.com>
2025-04-09 21:54:14 +0900

    loading

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -7,7 +7,7 @@ const LoadingPage = () => {
       <h3 className="loading-text">탄핵 심판 중...</h3>
       <div className="loader"></div>
       <p>헌법재판소가 당신의 사례를 검토하고 있습니다</p>
-      <div className="ad-space">광고 영역</div>
+      <div className="ad-space">예상보다 아주 오래 걸릴 수 있음...ㅎ..ㅎㅎ</div>
     </div>
   );
 };
