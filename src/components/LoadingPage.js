0fd06349552c18cb3aaaa972ab6c56ca4af9b319
moveit-moveit2 <lilycocochoi@gmail.com>
2025-04-10 10:25:06 +0900

    update

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -7,7 +7,13 @@ const LoadingPage = () => {
       <h3 className="loading-text">탄핵 심판 중...</h3>
       <div className="loader"></div>
       <p>헌법재판소가 당신의 사례를 검토하고 있습니다</p>
-      <div className="ad-space">예상보다 아주 오래 걸릴 수 있음...ㅎ..ㅎㅎ</div>
+      <div
+        className="ad-space"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <div>대선 2025.06.03(화) </div>
+        <div>사전투표 05.29(금) ~ 30(토)</div>
+      </div>
     </div>
   );
 };
